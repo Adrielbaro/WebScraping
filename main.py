aaac19843439b760062423fd4c44bcabb73d55a7
adrielbaron <adrielbaron2000@gmail.com>
2022-05-28 19:03:08 +0530

    Datas extracted from the website are now saved in an excel

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 from bs4 import BeautifulSoup
 import requests, openpyxl
 
+excel=openpyxl.Workbook()
+sheet=excel.active
+sheet.title="dam"
+#sheet.append(['Reservoirs','Full Depth','Full Capacity','Current Year Level','Current Year Storage','Current Year Inflow','Current Year Outflow','Last Year Level','Last Year Storage'])
+
 try:
     response=requests.get('https://tnagriculture.in/ARS/home/reservoir')
     soup=BeautifulSoup(response.text, 'html.parser')
-    #print(soup)
+    print(soup)
     datas=soup.find('tbody').find_all("tr")
 
     for data in datas:
-        print(data)
+        #print(data)
+        dam_1=data.find('tr' ,class_="bg-info").text
+        dam_2=data.find('tr', class_="bg-primary").text
+        print(dam_1)
+        sheet.append([dam_1,dam_2])
         break
+
 except Exception as e:
     print(e)
+
+excel.save("dams.xlsx")
